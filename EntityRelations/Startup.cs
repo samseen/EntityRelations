2eec297919032720eeaa1547857155804ae45cbe
Samson Akanbi <samson.akanbi@outlook.com>
2019-02-19 15:31:25 +0100

    Add project files.

---
+++ EntityRelations/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EntityRelations.Startup))]
+namespace EntityRelations
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
